7acfc8d111b23a9d2a5c29c856cb16f991d41e79
Frank Klomfas <frank_klomfas@directbox.com>
2024-12-10 12:50:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'magic-recipies',
+  webDir: 'www'
+};
+
+export default config;
